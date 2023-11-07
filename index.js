44b55b020ada95a7bb8eddbaf428e845b187c242
CodeSandbox <support@codesandbox.io>
2023-11-07 11:51:12 +0000

    Initial commit

--- index.js
+++ index.js
@@ -12,21 +12,264 @@ const cors = require("cors");
 const Middleware = require("./Routes/Middleware");
 app.use(bodyParser.json());
 app.use(
-  cors({
-    origin: "https://rzx75l-3000.csb.app",
-    credentials: true,
-    //  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    // allowedHeaders: ['Content-Type', 'Authorization'],
-  }),
+  cors(),
+  //   {
+  //   origin: "https://rzx75l-3000.csb.app",
+  //   credentials: true,
+  //   //  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  //   // allowedHeaders: ['Content-Type', 'Authorization'],
+  // }
 );
-
 app.use("/auth", Auth);
 app.use("/leaders", Leaders);
 app.use("/interns", Middleware, Present_interns);
 app.use("/pinterns", Middleware, Past_interns);
 app.use("/tinterns", Middleware, Terminated_interns);
 app.use("/quiz", Quiz);
-
+app.get("/music", (req, res) => {
+  res.json([
+    {
+      name: "track 1",
+      img: "",
+      tracks: [
+        {
+          title: "Death Bed",
+          artist: "Powfu",
+          artwork: "https://samplesongs.netlify.app/album-arts/death-bed.jpg",
+          url: "https://pagalworld.com.pe/files/download/id/6511",
+          id: "1",
+        },
+        {
+          title: "Bad Liar",
+          artist: "Imagine Dragons",
+          artwork: "https://samplesongs.netlify.app/album-arts/bad-liar.jpg",
+          url: "https://samplesongs.netlify.app/Bad%20Liar.mp3",
+          id: "2",
+        },
+        {
+          title: "Faded",
+          artist: "Alan Walker",
+          artwork: "https://samplesongs.netlify.app/album-arts/faded.jpg",
+          url: "https://samplesongs.netlify.app/Faded.mp3",
+          id: "3",
+        },
+        {
+          title: "Hate Me",
+          artist: "Ellie Goulding",
+          artwork: "https://samplesongs.netlify.app/album-arts/hate-me.jpg",
+          url: "https://samplesongs.netlify.app/Hate%20Me.mp3",
+          id: "4",
+        },
+        {
+          title: "Solo",
+          artist: "Clean Bandit",
+          artwork: "https://samplesongs.netlify.app/album-arts/solo.jpg",
+          url: "https://samplesongs.netlify.app/Solo.mp3",
+          id: "5",
+        },
+        {
+          title: "Without Me",
+          artist: "Halsey",
+          artwork: "https://samplesongs.netlify.app/album-arts/without-me.jpg",
+          url: "https://samplesongs.netlify.app/Without%20Me.mp3",
+          id: "6",
+        },
+      ],
+    },
+    {
+      name: "track 12",
+      img: "",
+      tracks: [
+        {
+          title: "Death Bed",
+          artist: "Powfu",
+          artwork: "https://samplesongs.netlify.app/album-arts/death-bed.jpg",
+          url: "https://pagalworld.com.pe/files/download/id/6511",
+          id: "1",
+        },
+        {
+          title: "Bad Liar",
+          artist: "Imagine Dragons",
+          artwork: "https://samplesongs.netlify.app/album-arts/bad-liar.jpg",
+          url: "https://samplesongs.netlify.app/Bad%20Liar.mp3",
+          id: "2",
+        },
+        {
+          title: "Faded",
+          artist: "Alan Walker",
+          artwork: "https://samplesongs.netlify.app/album-arts/faded.jpg",
+          url: "https://samplesongs.netlify.app/Faded.mp3",
+          id: "3",
+        },
+        {
+          title: "Hate Me",
+          artist: "Ellie Goulding",
+          artwork: "https://samplesongs.netlify.app/album-arts/hate-me.jpg",
+          url: "https://samplesongs.netlify.app/Hate%20Me.mp3",
+          id: "4",
+        },
+        {
+          title: "Solo",
+          artist: "Clean Bandit",
+          artwork: "https://samplesongs.netlify.app/album-arts/solo.jpg",
+          url: "https://samplesongs.netlify.app/Solo.mp3",
+          id: "5",
+        },
+        {
+          title: "Without Me",
+          artist: "Halsey",
+          artwork: "https://samplesongs.netlify.app/album-arts/without-me.jpg",
+          url: "https://samplesongs.netlify.app/Without%20Me.mp3",
+          id: "6",
+        },
+      ],
+    },
+    {
+      name: "Atrack 1Nkit",
+      img: "",
+      tracks: [
+        {
+          title: "Death Bed",
+          artist: "Powfu",
+          artwork: "https://samplesongs.netlify.app/album-arts/death-bed.jpg",
+          url: "https://pagalworld.com.pe/files/download/id/6511",
+          id: "1",
+        },
+        {
+          title: "Bad Liar",
+          artist: "Imagine Dragons",
+          artwork: "https://samplesongs.netlify.app/album-arts/bad-liar.jpg",
+          url: "https://samplesongs.netlify.app/Bad%20Liar.mp3",
+          id: "2",
+        },
+        {
+          title: "Faded",
+          artist: "Alan Walker",
+          artwork: "https://samplesongs.netlify.app/album-arts/faded.jpg",
+          url: "https://samplesongs.netlify.app/Faded.mp3",
+          id: "3",
+        },
+        {
+          title: "Hate Me",
+          artist: "Ellie Goulding",
+          artwork: "https://samplesongs.netlify.app/album-arts/hate-me.jpg",
+          url: "https://samplesongs.netlify.app/Hate%20Me.mp3",
+          id: "4",
+        },
+        {
+          title: "Solo",
+          artist: "Clean Bandit",
+          artwork: "https://samplesongs.netlify.app/album-arts/solo.jpg",
+          url: "https://samplesongs.netlify.app/Solo.mp3",
+          id: "5",
+        },
+        {
+          title: "Without Me",
+          artist: "Halsey",
+          artwork: "https://samplesongs.netlify.app/album-arts/without-me.jpg",
+          url: "https://samplesongs.netlify.app/Without%20Me.mp3",
+          id: "6",
+        },
+      ],
+    },
+    {
+      name: "track 122",
+      img: "",
+      tracks: [
+        {
+          title: "Death Bed",
+          artist: "Powfu",
+          artwork: "https://samplesongs.netlify.app/album-arts/death-bed.jpg",
+          url: "https://pagalworld.com.pe/files/download/id/6511",
+          id: "1",
+        },
+        {
+          title: "Bad Liar",
+          artist: "Imagine Dragons",
+          artwork: "https://samplesongs.netlify.app/album-arts/bad-liar.jpg",
+          url: "https://samplesongs.netlify.app/Bad%20Liar.mp3",
+          id: "2",
+        },
+        {
+          title: "Faded",
+          artist: "Alan Walker",
+          artwork: "https://samplesongs.netlify.app/album-arts/faded.jpg",
+          url: "https://samplesongs.netlify.app/Faded.mp3",
+          id: "3",
+        },
+        {
+          title: "Hate Me",
+          artist: "Ellie Goulding",
+          artwork: "https://samplesongs.netlify.app/album-arts/hate-me.jpg",
+          url: "https://samplesongs.netlify.app/Hate%20Me.mp3",
+          id: "4",
+        },
+        {
+          title: "Solo",
+          artist: "Clean Bandit",
+          artwork: "https://samplesongs.netlify.app/album-arts/solo.jpg",
+          url: "https://samplesongs.netlify.app/Solo.mp3",
+          id: "5",
+        },
+        {
+          title: "Without Me",
+          artist: "Halsey",
+          artwork: "https://samplesongs.netlify.app/album-arts/without-me.jpg",
+          url: "https://samplesongs.netlify.app/Without%20Me.mp3",
+          id: "6",
+        },
+      ],
+    },
+    {
+      name: "track 12",
+      img: "",
+      tracks: [
+        {
+          title: "Death Bed",
+          artist: "Powfu",
+          artwork: "https://samplesongs.netlify.app/album-arts/death-bed.jpg",
+          url: "https://pagalworld.com.pe/files/download/id/6511",
+          id: "1",
+        },
+        {
+          title: "Bad Liar",
+          artist: "Imagine Dragons",
+          artwork: "https://samplesongs.netlify.app/album-arts/bad-liar.jpg",
+          url: "https://samplesongs.netlify.app/Bad%20Liar.mp3",
+          id: "2",
+        },
+        {
+          title: "Faded",
+          artist: "Alan Walker",
+          artwork: "https://samplesongs.netlify.app/album-arts/faded.jpg",
+          url: "https://samplesongs.netlify.app/Faded.mp3",
+          id: "3",
+        },
+        {
+          title: "Hate Me",
+          artist: "Ellie Goulding",
+          artwork: "https://samplesongs.netlify.app/album-arts/hate-me.jpg",
+          url: "https://samplesongs.netlify.app/Hate%20Me.mp3",
+          id: "4",
+        },
+        {
+          title: "Solo",
+          artist: "Clean Bandit",
+          artwork: "https://samplesongs.netlify.app/album-arts/solo.jpg",
+          url: "https://samplesongs.netlify.app/Solo.mp3",
+          id: "5",
+        },
+        {
+          title: "Without Me",
+          artist: "Halsey",
+          artwork: "https://samplesongs.netlify.app/album-arts/without-me.jpg",
+          url: "https://samplesongs.netlify.app/Without%20Me.mp3",
+          id: "6",
+        },
+      ],
+    },
+  ]);
+});
 app.get("/", (req, res) => {
   res.json("API working");
 });
